d39c9ff4674f47d3fef581d4e12edcf970ab33fb
Rahul Kumar <rkrahulkv143@gmail.com>
2024-11-04 15:41:27 +0530

    mongo db connection set up

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 
